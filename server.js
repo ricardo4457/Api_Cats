cdf30b9d32a589a904e4ac2b2f92ad94b80959f2
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-03-26 00:21:14 +0000

    feat : preparing server.js to run all routes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const http = require('http');
-const app = require('./app');
 
-const port =  process.env.PORT || 3000
+const express = require('express');
+const catRoutes = require('./routes/catRoutes');
 
-const server = http.createServer(app);
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-server.listen(port);
+app.use(express.json());
+
+app.use('/api/v1', catRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
