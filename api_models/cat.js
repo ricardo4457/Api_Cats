74adecd8510e662cf2e6f7350e191068f80a839a
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-03-28 20:20:35 +0000

    fix : create api_models folder to avoid error model sequilize

---
+++ api_models/cat.js
@@ -0,0 +1,11 @@
+class Cat {
+   constructor(id, tags, mimetype, createdAt, imageUrl) {
+     this.id = id;
+     this.tags = tags;
+     this.mimetype = mimetype;
+     this.createdAt = createdAt;
+     this.imageUrl = imageUrl; 
+   }
+ }
+
+module.exports = Cat;
